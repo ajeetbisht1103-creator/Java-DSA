1c0f30093c6362a7531bff8e86c710c799dd7d30
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-07-13 23:32:20 +0530

    Created Second Smallest and Second Largest Element template

---
+++ Arrays/SecondSmallestAndSecondLargestElement.java
@@ -0,0 +1,20 @@
+/**
+ * Problem: Find Second Smallest and Second Largest Element in an Array
+ *
+ * Description:
+ * Find the second smallest and second largest elements
+ * in a given array.
+ *
+ * Time Complexity:
+ * To be added
+ *
+ * Space Complexity:
+ * To be added
+ */
+
+public class SecondSmallestAndSecondLargestElement {
+
+    public static void main(String[] args) {
+
+    }
+}
