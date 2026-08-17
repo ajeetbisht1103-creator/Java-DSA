50a5cbd3d9f23faf29ec75343c46d119255a2513
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-08-17 20:08:33 +0530

    Added Second Smallest and Second Largest Element

--- Arrays/SecondSmallestAndSecondLargestElement.java
+++ Arrays/SecondSmallestAndSecondLargestElement.java
@@ -6,15 +6,77 @@
  * in a given array.
  *
  * Time Complexity:
- * To be added
+ * O(N)
  *
  * Space Complexity:
- * To be added
+ * O(1)
  */
+
+class Solution {
+
+    public static int findSecondSmallest(int[] array, int n) {
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        if (n < 2) {
+            return -1;
+        }
+
+        for (int index = 0; index < n; index++) {
+
+            if (array[index] < smallest) {
+                secondSmallest = smallest;
+                smallest = array[index];
+            }
+            else if (array[index] < secondSmallest
+                    && array[index] != smallest) {
+                secondSmallest = array[index];
+            }
+        }
+
+        return secondSmallest;
+    }
+
+    public static int findSecondLargest(int[] array, int n) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        if (n < 2) {
+            return -1;
+        }
+
+        for (int index = 0; index < n; index++) {
+
+            if (array[index] > largest) {
+                secondLargest = largest;
+                largest = array[index];
+            }
+            else if (array[index] > secondLargest
+                    && array[index] != largest) {
+                secondLargest = array[index];
+            }
+        }
+
+        return secondLargest;
+    }
+}
 
 public class SecondSmallestAndSecondLargestElement {
 
     public static void main(String[] args) {
 
+        int[] array = {1, 2, 4, 7, 7, 5};
+        int n = array.length;
+
+        int secondSmallest =
+                Solution.findSecondSmallest(array, n);
+
+        int secondLargest =
+                Solution.findSecondLargest(array, n);
+
+        System.out.println("Second smallest is " + secondSmallest);
+        System.out.println("Second largest is " + secondLargest);
     }
 }
