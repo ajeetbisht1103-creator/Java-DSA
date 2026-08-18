8456d9b6daee5adb67e0df51b4ed3d3be45a9ec5
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-08-18 11:54:56 +0530

    Your commit message

---
+++ Arrays/CheckIfArrayIsSorted.java
@@ -0,0 +1,49 @@
+/**
+ * Problem: Check if an Array is Sorted
+ *
+ * Description:
+ * Check whether the given array is sorted in
+ * non-decreasing (ascending) order.
+ *
+ * Time Complexity:
+ * O(N)
+ *
+ * Space Complexity:
+ * O(1)
+ */
+
+class Solution {
+
+    /**
+     * Checks whether the array is sorted in non-decreasing order.
+     */
+    public static boolean isSorted(int[] array, int n) {
+
+        for (int index = 1; index < n; index++) {
+
+            if (array[index] < array[index - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class CheckIfArrayIsSorted {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 4, 4, 3, 8};
+        int n = array.length;
+
+        boolean sorted = Solution.isSorted(array, n);
+
+        if (sorted) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
