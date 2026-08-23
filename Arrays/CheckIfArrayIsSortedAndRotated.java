d1088123a02eab4a0bedaf7348c7dba2659b57e8
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-08-23 20:26:34 +0530

    Added Check If Array Is Sorted and Rotated

---
+++ Arrays/CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,38 @@
+/**
+ * Problem Statement: Given an array nums, return true if the array was originally sorted
+ * in non-decreasing order, then rotated some number of positions (including zero).
+ * Otherwise, return false.
+ *
+ * Time Complexity: O(N) - Single pass through the array.
+ * Space Complexity: O(1) - Constant auxiliary space.
+ */
+public class CheckIfArrayIsSortedAndRotated {
+
+    public static boolean check(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return true;
+        }
+
+        int n = nums.length;
+        int countBreaks = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                countBreaks++;
+            }
+            if (countBreaks > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {3, 4, 5, 1, 2};
+        int[] arr2 = {2, 1, 3, 4};
+
+        System.out.println("Is {3, 4, 5, 1, 2} sorted and rotated? " + check(arr1));
+        System.out.println("Is {2, 1, 3, 4} sorted and rotated? " + check(arr2));
+    }
+}
