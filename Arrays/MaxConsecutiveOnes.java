b24a965ac77b0aaa00c6c15aad48c0e41d8cb7e3
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-09-03 20:45:12 +0530

    Added Max Consecutive Ones in Java

---
+++ Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+class MaxConsecutiveOnes{
+    public static int maxConsecutiveOnes(int[] arr){
+    int count1=0;
+    int max=0;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==1)
+        {
+            count1++;
+        }
+        else
+        {
+            count1=0;
+        }
+        max=Math.max(count1,max);
+    }
+    return max;
+    }
+        public static void main(String [] args){
+        int[] arr={1, 1, 0, 1, 1, 1};
+        System.out.println(maxConsecutiveOnes(arr));
+    }
+}
+
